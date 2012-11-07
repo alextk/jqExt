e8ef6748c9f88ab1ef3edfcca7f4f10f5b00b797
Alex <tkachev.alex@gmail.com>
2012-11-07 13:22:15 +0200

    added next/prev month and strftime to date

--- src/core/date.js
+++ src/core/date.js
@@ -1,4 +1,5 @@
 jQuery.extend(Date.prototype, /** @scope Date */{
+  locale: 'en-US',
 
   /**
    * @function {public long} ?
@@ -6,8 +7,71 @@ jQuery.extend(Date.prototype, /** @scope Date */{
    * @param {optional Date} from - calculate elapsed miliseconds from this date to given from date. Defaults to now date.
    * @returns number of miliseconds between given date and this date. if date is not given, return number of miliseconds elapsed from now
    */
-  getElapsed: function(from) {
-    return Math.abs((from || new Date()).getTime()-this.getTime());
+  getElapsed: function(from){
+    return Math.abs((from || new Date()).getTime() - this.getTime());
+  },
+
+  /**
+   * @function {public Date} ?
+   * Returns date of the next month (if today is 25/12/2012 --> 01/01/2013)
+   */
+  nextMonth: function(){
+    if(this.getMonth() == 11){
+      return new Date(this.getFullYear() + 1, 1, 1);
+    }else{
+      return new Date(this.getFullYear(), this.getMonth() + 1, 1);
+    }
+  },
+
+  /**
+   * @function {public Date} ?
+   * Returns date of the previous month (if today is 25/01/2012 --> 01/12/2011)
+   */
+  prevMonth: function(){
+    if(this.getMonth() == 0){
+      return new Date(this.getFullYear() - 1, 11, 1);
+    }else{
+      return new Date(this.getFullYear(), this.getMonth() - 1, 1);
+    }
+  },
+
+  /**
+   * @function {public Date} ?
+   * Formats current date by given strftime pattern and returns resulting string
+   * (see man strftime or http://hacks.bluesmoon.info/strftime/demo.html)
+   */
+  strftime: function(format){
+    // Fix locale if declared locale hasn't been defined
+    // After the first call this condition should never be entered unless someone changes the locale
+    if(!(this.locale in Date.strftime_helper.locales)){
+      this.locale = 'en-US';
+    }
+
+    var d = this;
+    // First replace aggregates
+    while(format.match(/%[cDhnrRtTxXzZ]/)){
+      format = format.replace(/%([cDhnrRtTxXzZ])/g, function(m0, m1){
+        var f = Date.strftime_helper.aggregates[m1];
+        return (f == 'locale' ? Date.strftime_helper.locales[d.locale][m1] : f);
+      });
+    }
+
+
+    // Now replace formats - we need a closure so that the date object gets passed through
+    var str = format.replace(/%([aAbBCdegGHIjmMpPSuUVwWyY%])/g, function(m0, m1){
+      var f = Date.strftime_helper.formats[m1];
+      if(typeof(f) == 'string'){
+        return d[f]();
+      }else if(typeof(f) == 'function'){
+        return f.call(d, d);
+      }else if(typeof(f) == 'object' && typeof(f[0]) == 'string'){
+        return Date.strftime_helper.util.xPad(d[f[0]](), f[1]);
+      }else{
+        return m1;
+      }
+    });
+    d = null;
+    return str;
   }
 
 });
---
+++ test/unit/core/date-test.js
@@ -0,0 +1,39 @@
+module("Core - date.js");
+
+test("Basic requirements", function() {
+});
+
+test("nextMonth()", function() {
+  var nextMonth = null;
+
+  nextMonth = new Date(2012, 7, 2).nextMonth();
+  equal(nextMonth.getFullYear(), 2012);
+  equal(nextMonth.getMonth(), 8);
+  equal(nextMonth.getDate(), 1);
+
+  nextMonth = new Date(2012, 11, 12).nextMonth();
+  equal(nextMonth.getFullYear(), 2013);
+  equal(nextMonth.getMonth(), 1);
+  equal(nextMonth.getDate(), 1);
+});
+
+
+test("prevMonth()", function() {
+  var prevMonth = null;
+
+  prevMonth = new Date(2012, 7, 2).prevMonth();
+  equal(prevMonth.getFullYear(), 2012);
+  equal(prevMonth.getMonth(), 6);
+  equal(prevMonth.getDate(), 1);
+
+  prevMonth = new Date(2012, 0, 15).prevMonth();
+  equal(prevMonth.getFullYear(), 2011);
+  equal(prevMonth.getMonth(), 11);
+  equal(prevMonth.getDate(), 1);
+});
+
+
+
+test("strftime()", function() {
+  equal(new Date(2012, 7, 2).strftime('%d-%m-%Y'), '02-08-2012');
+});
